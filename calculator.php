ee45b15ab4f2e3de14c69f1243a557a3a54ae65b
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-03 12:20:25 +0300

    building a basic calc

---
+++ calculator.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic calculator</title>
+</head>
+<body>
+
+    <h2>Building a nasic calculator</h2>
+
+    <form action='calculator.php' method='get'>
+
+        <label>Enter the first number</label>
+        <input type='text' name='num-1'/>
+        <br>
+
+        <label>Enter the second number</label>
+        <input type='text' name='num-2'/>
+
+        <br>
+        <button type='submit'>Calculate</button>
+
+
+
+    </form>
+
+    <?php echo $_GET["num-1"] + $_GET["num-2"]?>
+    
+</body>
+</html>
