edf1f4c403dc04b2f4cf3a8acee72ad9fbca87e0
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-03 11:01:45 +0300

    starting with PHP

--- site.php
+++ site.php
@@ -10,7 +10,21 @@
     <h1>PHP</h1>
     <?php
 
+    $charactername = "Wanjie";
+    $characterAge = 55;
+
+    $num_1 = 2;
+    $num_3 = 4;
+    $result = $num_1 + $num_3;
+
     echo("Hello World!");
+    echo "<hr>";
+    echo "<h2>PHP starter</h2>";
+    echo "<p>Stop joking freind</p>";
+    // help write info to the html docs 
+
+    echo "My name is $charactername and I am $characterAge years old";
+    echo "The result is $result";
     
     
     ?>
