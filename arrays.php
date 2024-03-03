c701983863810aaa2912d6d6cf759eb840d9953f
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-03-03 15:28:59 +0300

    working with arrays

---
+++ arrays.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Working with Arrays</title>
+</head>
+
+<body>
+
+    <h2>Working with Arrays</h2>
+
+    <?php
+
+    $friends = array("Kev", "Caren", "ryan", "joy", 45, 60, 'xxx');
+    echo $friends[1];
+    echo $friends[5];
+    echo $friends[6];
+
+    ?>
+
+
+</body>
+
+</html>
